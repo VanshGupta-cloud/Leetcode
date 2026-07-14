d9bbea94ce2ba4ba03b44dfd61bb40b0bcc5b45f
VanshGupta-cloud <hardworker08642@gmail.com>
2026-07-14 22:20:26 +0530

    Time: 3 ms (92.52%) | Memory: 44.4 MB (89.87%) - LeetSync

--- 1044-find-common-characters/find-common-characters.java
+++ 1044-find-common-characters/find-common-characters.java
@@ -1,35 +1,35 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Arrays;
-
 class Solution {
     public List<String> commonChars(String[] words) {
-        // Step 1: Create our "master" hash array and fill it with max values
-        int[] minFreq = new int[26];
-        Arrays.fill(minFreq, Integer.MAX_VALUE);
-        
-        // Step 2: Hash the frequency of characters for each word
-        for (String word : words) {
-            int[] charCount = new int[26]; // Fast hash array for the current word
-            for (char ch : word.toCharArray()) {
-                charCount[ch - 'a']++; // Hashing and incrementing the count
+        int []minfreq=new int[26];
+        for(int i=0;i<26;i++)
+        {
+            minfreq[i]=Integer.MAX_VALUE;
+        }
+        int n=words.length;
+        for(int i=0;i<n;i++)
+        {
+            int []freq=new int[26];
+            String s=words[i];
+            char[]str=s.toCharArray();
+            for(int j=0;j<str.length;j++)
+            {
+               freq[str[j]-97]++;
             }
             
-            // Step 3: Intersect frequencies by keeping the minimum values
-            for (int i = 0; i < 26; i++) {
-                minFreq[i] = Math.min(minFreq[i], charCount[i]);
+            for(int j=0;j<26;j++)
+            {
+                minfreq[j]=Math.min(minfreq[j],freq[j]);
             }
         }
-        
-        // Step 4: De-hash back into the resulting string list
-        List<String> result = new ArrayList<>();
-        for (int i = 0; i < 26; i++) {
-            while (minFreq[i] > 0) {
-                result.add(String.valueOf((char)(i + 'a')));
-                minFreq[i]--;
-            }
+            
+       ArrayList<String>List=new ArrayList<>();
+        for(int i=0;i<26;i++)
+        { while(minfreq[i]>0){
+            List.add(String.valueOf((char)(i+97)));
+          minfreq[i]--;
+          }
         }
+        return List;
         
-        return result;
     }
 }
