798c5138463a7334b6692c52401602d3504553ba
VanshGupta-cloud <hardworker08642@gmail.com>
2026-07-14 19:44:08 +0530

    Time: 2 ms (97.68%) | Memory: 45.2 MB (25.45%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,32 +1,33 @@
+import java.util.HashMap;
+import java.util.ArrayList;
+
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-
-      HashSet <Integer> set= new HashSet<>();
-      HashSet <Integer> intersect=new HashSet<>();
-
-      for(int num:nums1)
-      {
-        set.add(num);
-      }
-      for(int num:nums2)
-      {
-        if(set.contains(num))
-        {
-            intersect.add(num);
+        // Map will store: Key = the number, Value = has it been added to the result?
+        HashMap<Integer, Boolean> map = new HashMap<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        // 1. Put all numbers from nums1 into the map. 
+        // We set the value to false because it hasn't been added to the intersection yet.
+        for (int num : nums1) {
+            map.put(num, false);
         }
-      }
-      int []result=new int[intersect.size()];
-      int count=0;
-      for(int num:intersect)
-      {
-        result[count++]=num;
-      }
-
-      return result;
-
-
-
-
+        
+        // 2. Check elements of nums2 against the map
+        for (int num : nums2) {
+            // If the map contains the key AND we haven't already added it to the result
+            if (map.containsKey(num) && map.get(num) == false) {
+                list.add(num);
+                map.put(num, true); // Mark as true so we don't add duplicates from nums2
+            }
+        }
+        
+        // 3. Convert the ArrayList back to a primitive int[] array
+        int[] result = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+        
+        return result;
     }
 }
-    
