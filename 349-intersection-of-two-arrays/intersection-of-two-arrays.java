dfc967a3a9f4dd7f462681a267524c48981e5948
VanshGupta-cloud <hardworker08642@gmail.com>
2026-07-14 19:51:24 +0530

    Time: 2 ms (97.68%) | Memory: 45 MB (41.32%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,39 +1,32 @@
-import java.util.HashMap;
-
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        // Map stores: Key = Number, Value = State code
-        // State 1: Present in nums1
-        // State 2: Present in both nums1 and nums2 (Intersection found)
-        HashMap<Integer, Integer> map = new HashMap<>();
-        
-        // 1. Mark all elements of nums1 with state 1
-        for (int num : nums1) {
-            map.put(num, 1);
+
+      HashSet <Integer> set= new HashSet<>();
+      HashSet <Integer> intersect=new HashSet<>();
+
+      for(int num:nums1)
+      {
+        set.add(num);
+      }
+      for(int num:nums2)
+      {
+        if(set.contains(num))
+        {
+            intersect.add(num);
         }
-        
-        int intersectionCount = 0;
-        
-        // 2. Scan nums2 to identify matches and count unique intersections
-        for (int num : nums2) {
-            Integer state = map.get(num);
-            if (state != null && state == 1) {
-                map.put(num, 2); // Upgrade state to 2 (Matched)
-                intersectionCount++; // Increment counter so we know the exact array size
-            }
-        }
-        
-        // 3. Directly create the perfectly-sized primitive array
-        int[] result = new int[intersectionCount];
-        int index = 0;
-        
-        // 4. Fill the array by collecting keys that reached State 2
-        for (int num : map.keySet()) {
-            if (map.get(num) == 2) {
-                result[index++] = num;
-            }
-        }
-        
-        return result;
+      }
+      int []result=new int[intersect.size()];
+      int count=0;
+      for(int num:intersect)
+      {
+        result[count++]=num;
+      }
+
+      return result;
+
+
+
+
     }
 }
+    
