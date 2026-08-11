3310b90bd62360dcc519085701bd78f1e0cc60f0
VanshGupta-cloud <hardworker08642@gmail.com>
2026-08-11 11:41:14 +0530

    Time: 3 ms (53.06%) | Memory: 55.8 MB (32.27%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -3,14 +3,16 @@ class Solution {
         int n = nums.length;
         int num = nums[0];
         int count = 1;
+        //Boyer-Moore's Majority Vote Algorithm
         for (int i = 1; i < nums.length; i++) {
-            if (count == 0) {
-                num = nums[i];
-            }
             if (nums[i] == num) {
                 count++;
             } else {
                 count--;
+            }
+             if (count == 0) {
+                num = nums[i];
+                count=1;
             }
         }
         return num;
