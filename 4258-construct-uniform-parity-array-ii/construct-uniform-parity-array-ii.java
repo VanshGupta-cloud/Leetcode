0d0c5f38e1e6ebb72f56f15d693298e924315e39
VanshGupta-cloud <hardworker08642@gmail.com>
2026-09-05 12:04:31 +0530

    Time: 9 ms (24.85%) | Memory: 121.6 MB (66.36%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
@@ -12,27 +12,22 @@ class Solution {
         }
         if (minodd == Integer.MAX_VALUE) {
             return true;
-        }
-      else{
-        int count=0;
-        for(int i=0;i<n;i++)
-        {
-            if(nums1[i]%2!=0)
-            {
-             count++;
-            }
-            else
-            {
-                int p = nums1[i] - minodd;
-                if (p >= 1) {
-                   count++;
+        } else {
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                if (nums1[i] % 2 != 0) {
+                    count++;
+                } else {
+                    int p = nums1[i] - minodd;
+                    if (p >= 1) {
+                        count++;
+                    }
                 }
             }
+            if (count == n) {
+                return true;
+            }
         }
-        if(count == n) {
-            return true;
-        }
-      }
         return false;
     }
 }
