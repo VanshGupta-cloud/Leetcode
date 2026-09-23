bcd2bb2bfbeea765d520c90dd97f8bd306e46c9b
VanshGupta-cloud <hardworker08642@gmail.com>
2026-09-23 22:16:57 +0530

    Time: 1 ms (100.00%) | Memory: 44.2 MB (81.27%) - LeetSync

--- 4053-majority-frequency-characters/majority-frequency-characters.java
+++ 4053-majority-frequency-characters/majority-frequency-characters.java
@@ -1,42 +1,35 @@
 class Solution {
     public String majorityFrequencyGroup(String s) {
         //freq counting of characters
-      int[]charfreq=new int[26];
-      int n=s.length();
-      for(int i=0;i<n;i++)
-      {
-        charfreq[s.charAt(i)-97]++;
-      }
-      //store the same freq count together
-      int[]storefreq=new int[n+1];
-      for(int i=0;i<charfreq.length;i++)
-      {
-        if(charfreq[i]>0)
-        {
-            storefreq[charfreq[i]]++;
+        int[] charfreq = new int[26];
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            charfreq[s.charAt(i) - 97]++;
         }
-      }
-      //counts the maxlen string with common freq and if two or more same then bestfreq one out of them
-      int bestfreq=0;
-      int maxlen=0;
-      for(int freq=1;freq<=s.length();freq++)
-      {
-        
-        if(maxlen<storefreq[freq]||maxlen==storefreq[freq]&&bestfreq<freq)
-        {
-           maxlen=storefreq[freq];
-           bestfreq=freq;
+        //store the same freq count together
+        int[] storefreq = new int[n + 1];
+        for (int i = 0; i < charfreq.length; i++) {
+            if (charfreq[i] > 0) {
+                storefreq[charfreq[i]]++;
+            }
         }
-      }
-      //every character matching the frequency of bestfreq is part of ans ,as bestfreq sticks with maxlength
-      StringBuilder sb=new StringBuilder();
-       for(int i=0;i<26;i++)
-       {
-        if(charfreq[i]==bestfreq)
-        {
-            sb.append((char)(97+i));
+        //counts the maxlen string with common freq and if two or more same then bestfreq one out of them
+        int bestfreq = 0;
+        int maxlen = 0;
+        for (int freq = 1; freq <= s.length(); freq++) {
+
+            if (maxlen < storefreq[freq] || maxlen == storefreq[freq] && bestfreq < freq) {
+                maxlen = storefreq[freq];
+                bestfreq = freq;
+            }
         }
-       }
-       return sb.toString();
+        //every character matching the frequency of bestfreq is part of ans ,as bestfreq sticks with maxlength
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 26; i++) {
+            if (charfreq[i] == bestfreq) {
+                sb.append((char) (97 + i));
+            }
+        }
+        return sb.toString();
     }
 }
