c2b8afdf45ab03702e52c7019ecdccd34c0cfad2
VanshGupta-cloud <hardworker08642@gmail.com>
2026-09-24 22:30:21 +0530

    Time: 2 ms (79.93%) | Memory: 44 MB (22.41%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.java
+++ 1666-make-the-string-great/make-the-string-great.java
@@ -1,24 +1,19 @@
 class Solution {
     public String makeGood(String s) {
-        int n=s.length();
-        Deque <Character> stack=new ArrayDeque<>();
-        for(int i=0;i<n;i++)
-        {
-            char ch=s.charAt(i);
-          if(!stack.isEmpty()&&Math.abs(stack.peek()-ch)==32)
-          {
-            stack.pop();
-          }
-          else
-          {
-            stack.push(ch);
-          }
+        int n = s.length();
+        Deque<Character> stack = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (!stack.isEmpty() && Math.abs(stack.peek() - ch) == 32) {
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
         }
-        StringBuilder sb=new StringBuilder();
-        while(!stack.isEmpty())
-        {
-          sb.append(stack.pop());
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
         }
-    return sb.reverse().toString();
+        return sb.reverse().toString();
     }
 }
